9f3455887e7deddb90f5b3fc56aa0d50c2a091cc
MahaDaiki <mahadaiki2@gmail.com>
2023-10-26 20:56:10 +0100

    something

--- assets/index.js
+++ assets/index.js
@@ -9,3 +9,32 @@ document.addEventListener('DOMContentLoaded', function() {
         // movingImage.style.top = mouseY + 'px';
     });
 });
+
+const rasImage = document.getElementById('ras');
+
+        let ballY = 0;// Initial position of the "ras" image
+        let ballX = 0;
+        let verticalVelocity = 0; // Initial vertical velocity
+        const gravity = .2; // Gravity value
+
+        function moveBall() {
+            // Apply gravity to the vertical velocity
+            verticalVelocity += gravity;
+
+            // Update the position of the "ras" image
+            ballY += verticalVelocity;
+            rasImage.style.top = ballY + "px";
+            ballX += verticalVelocity;
+            rasImage.style.top = ballX + "px";
+
+            // Check for collisions with the canvas floor
+            if (ballY + rasImage.clientHeight > window.innerHeight) {
+                ballY = window.innerHeight - rasImage.clientHeight;
+                verticalVelocity = -verticalVelocity; // Reverse the vertical direction
+            }
+
+            requestAnimationFrame(moveBall);
+        }
+        moveBall();
+
+        // Call the function to start making the "ras" image fall
